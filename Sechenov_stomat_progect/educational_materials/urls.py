09ad822b23f3ad9ae3ad8deaaa769c73b8ca4d3f
Климов Степан <stepklim05@mail.ru>
2024-03-02 14:00:59 +0300

    creae anatomia structure

--- Sechenov_stomat_progect/educational_materials/urls.py
+++ Sechenov_stomat_progect/educational_materials/urls.py
@@ -3,4 +3,5 @@ from educational_materials import views
  
 urlpatterns = [
     path('', views.first_list_educational_materials, name='f_l_e_m'),
+    path('anatomia/', views.anatomia, name='anatomia')
 ]
--- Sechenov_stomat_progect/educational_materials/views.py
+++ Sechenov_stomat_progect/educational_materials/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def first_list_educational_materials(request):
     return render(request, 'educational_materials/f_l_e_m.html')
 
+def anatomia(request):
+    return render(request, 'educational_materials/anatomia.html')
+
+
